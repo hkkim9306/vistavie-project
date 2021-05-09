a9b775bb6d9a3bb68427727850e06bedf99bdcdd
tenderpooh <tenderpooh@gmail.com>
2021-05-10 02:07:08 +0900

    답변 달아드립니다.
    유튜브 영상 확인해보세요~

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const abc = 1;
 
 console.log(abc);
 
-
 //강사님 너무 어려워요 ㅜㅜ
+
+//ㅇㅏ아~ 아래 유튜브 주소를 참고해서 한번 해보는건 어떨까요?
+
+// https://github.com/hkkim9306/vistavie-project/compare/02ab145f3f3c...81cc058101af
